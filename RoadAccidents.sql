0f696b9305606b128692610cbd33d21c8d8135b7
Suriya G <suriyagurumoorthi02@gmail.com>
2023-11-18 10:18:19 +0530

    RoadAccidents.sql

--- RoadAccidents.sql
+++ RoadAccidents.sql
@@ -6,6 +6,7 @@ group by area;
 
 
 -- Question 2: Which day of the week has the highest number of accidents?
+
 select day, count(accidentindex) as count
 from accident
 group by day
@@ -13,6 +14,7 @@ order by count desc;
 
 
 -- Question 3: What is the average age of vehicles involved in accidents based on their type?
+
 select vehicletype, round(avg(agevehicle)) as average_age
 from vehicles
 where agevehicle is not null
@@ -20,6 +22,7 @@ group by vehicletype
 order by average_age desc ;
 
 -- Question 4: Can we identify any trends in accidents based on the age of vehicles involved?
+
 select 
 	AgeGroup,
 	count(AccidentIndex) as 'Total Accident',
@@ -37,7 +40,8 @@ from (
 ) as SubQuery
 group by 
 	AgeGroup;
-    
+
+
 -- Question 5: Are there any specific weather conditions that contribute to severe accidents?
 
 
@@ -56,11 +60,13 @@ order by
 	'Total Accident' desc;
 
 -- Question 6: Do accidents often involve impacts on the left-hand side of vehicles?
+
 select lefthand, count(accidentindex) as 'Accident Count'
 from vehicles
 group by lefthand;
 
 -- Question 7: Are there any relationships between journey purposes and the severity of accidents?
+
 select journeypurpose, count(severity) as 'Accident Count',
 case
 when count(severity) between 0 and 1000 then 'Low'
